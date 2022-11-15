31ada1e2e84c6176110c8d599350b26667e9705e
Carmenla2909 <98791446+Carmenla2909@users.noreply.github.com>
2022-11-15 12:01:24 -0600

    Add files via upload

---
+++ carmen/menu.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar bg-light-dark bg-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand mb-2 " href="#"><p class="fw-bolder fst-italic text.">SISTEMA DIF SAN JUAN XIUTETELCO</a></p>
+    <img src="https://raw.githubusercontent.com/mxabierto/assets/master/img/logos/sndif.png" alt="DIF" width="110" height="110">
+    <form class="d-flex" role="search">
+      <input class="form-control me-1" type="search" placeholder="Buscar Informacion" aria-label="Search">
+      <button class="btn btn-success" type="submit">Buscar</button>
+    </form>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Informacion</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Servicios
+          </a>
+         </ul>
+      </div>
+  </div>
+</nav>
