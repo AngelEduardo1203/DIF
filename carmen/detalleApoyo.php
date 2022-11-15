f3420f16b487f5f474ab38b69e4db019304e7f64
Carmenla2909 <98791446+Carmenla2909@users.noreply.github.com>
2022-11-15 11:58:57 -0600

    Add files via upload

---
+++ carmen/detalleApoyo.php
@@ -0,0 +1,10 @@
+<div class="container text-center">
+	<ul class="list-group">
+<li class="list-group-item">LISTA DE LOS APOYOS</li>
+
+<li class="list-group-item  bg-danger text-black"><p>Nombre Apoyo : <?=$apoyo[0]->Nombre_Apoyo ?></p></li>
+<li class="list-group-item  bg-danger text-black"><p>Fecha Entrega : <?=$apoyo[0]->Fecha_Entrega?></p></li>
+<li class="list-group-item  bg-danger text-black"><p>Nombre Beneficiario : <?=$apoyo[0]->Nombre_Beneficiario ?></p></li>
+<li class="list-group-item  bg-danger text-black"><p>Nombre Tutor : <?=$apoyo[0]->Nombre_Tutor ?></p></li>
+<li class="list-group-item  bg-danger text-black"><p>Descripcion : <?=$apoyo[0]->Descripcion ?></p></li>
+</div>
